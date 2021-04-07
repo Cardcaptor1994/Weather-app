476e67a15b150d8b465fe24ea975dede61a0769a
Cardcaptor1994 <53077707+Cardcaptor1994@users.noreply.github.com>
2021-04-07 22:22:26 +0530

    Update app.js

--- Website/app.js
+++ Website/app.js
@@ -8,7 +8,7 @@ let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // Personal API Key for OpenWeatherMap API
 
-const apiKey = '0b8fc555226b242d10b77d10d2d8d8c4=metric';
+const apiKey = '0b8fc555226b242d10b77d10d2d8d8c4&units=metric';
 
 // Event listener to add function to existing HTML DOM element
 document.addEventListener("DOMContentLoaded", function(){
